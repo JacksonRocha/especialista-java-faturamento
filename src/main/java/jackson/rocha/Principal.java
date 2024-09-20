de765e1817e83cc4ba5f57730f17d397720c4e28
jacksonrocha <jackson_lbr@hotmail.com>
2024-09-19 22:19:44 -0300

    Classes abstratas

--- src/main/java/jackson/rocha/Principal.java
+++ src/main/java/jackson/rocha/Principal.java
@@ -7,6 +7,7 @@ import jackson.rocha.fiscal.NotaFiscalServico;
 public class Principal {
     public static void main(String[] args) {
         var gestorFiscal = new GestorFiscal();
+        
         var nfBolaFutebol = new NotaFiscalProduto("Bola Futebol", 300, 50);
         var nfReparoMotor = new NotaFiscalServico("Reparo da roda", 1100, true);
 
